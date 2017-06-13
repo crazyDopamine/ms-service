69133d24dda32a9ef7409407acc36ae8581e97d8
dongwei <dongwei@1b2b.cn>
2017-06-13 16:23:25 +0800

    fiel

--- routes/file.js
+++ routes/file.js
@@ -4,12 +4,12 @@ var global = require('../global');
 var common = require('./common');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({uploadDir:'./file' });
-var localhost = 'http://116.62.23.38/';
+var localhost = 'http://116.62.23.38:3000/';
 router.use(multipart({uploadDir:'./file' }));
 router.post('/editorUpload', multipartMiddleware, function(req, res) {
-    console.log(req.headers.host);
+    console.log(req.headers);
     // don't forget to delete all req.files when done
-    res.json({link:localhost+req.files.file.path});
+    res.json({link:'http://'+req.headers.host+'/'+req.files.file.path});
 });
 
 module.exports = router;
