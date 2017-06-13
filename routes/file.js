0e66a3e085bbbe57eebc2f17075dfa15f33d402a
dongwei <dongwei@1b2b.cn>
2017-06-13 15:47:21 +0800

    file

--- routes/file.js
+++ routes/file.js
@@ -4,11 +4,12 @@ var global = require('../global');
 var common = require('./common');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({uploadDir:'./file' });
+var localhost = 'http://116.62.23.38/';
 router.use(multipart({uploadDir:'./file' }));
 router.post('/editorUpload', multipartMiddleware, function(req, res) {
     console.log(req.headers.host);
     // don't forget to delete all req.files when done
-    res.json({link:'http://'+req.headers.host+'/'+req.files.file.path});
+    res.json({link:localhost+req.files.file.path});
 });
 
 module.exports = router;
